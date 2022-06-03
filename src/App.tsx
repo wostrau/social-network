2405498e2448c51494003943b9757916338a094f
wostrau <a.ostrovtsov@gmail.com>
2022-06-03 11:59:22 +0300

    import {listFormat} was deleted from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { ListFormat } from "typescript";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Navbar from "./components/Navbar/Navbar";
