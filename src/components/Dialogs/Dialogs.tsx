7b772edc28250c0acdef7f86383ad66abc26283d
wostrau <a.ostrovtsov@gmail.com>
2022-06-30 23:51:34 +0300

    added .map method to picture dialogs and messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,20 +4,14 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 const Dialogs = (props: any) => {
+    const dialogElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    const messageElements = props.state.messages.map(m => <Message id={m.id} message={m.message}/>);
+
     return (
         <div className={styles.dialogs}>
-            <DialogItem id="1" name="Sasha" dialog={props.state.dialogs}/>
-            <DialogItem id="2" name="Maksim" dialog={props.state.dialogs}/>
-            <DialogItem id="3"  name="Ivan" dialog={props.state.dialogs}/>
-
+            {dialogElements}
             <div className={styles.messages}>
-                <Message id='1' message={props.state.messages}/>
-                <Message id='2' message={props.state.messages}/>
-                <Message id='3' message={props.state.messages}/>
-
-
-                <div className={styles.message}>HI</div>
-                <div className={styles.message}>HOI</div>
+                {messageElements}
             </div>
         </div>
     );
