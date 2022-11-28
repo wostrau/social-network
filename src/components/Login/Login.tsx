7edf48e58539d550c66f586da91c37e9975a1b43
wostrau <a.ostrovtsov@gmail.com>
2022-11-28 17:06:53 +0100

    lesson #75 from playlist: samurai way - DONE / topic: redux-form introduction

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+
+const LoginForm = (props: any) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field
+                    name={'login'}
+                    placeholder={'Login'}
+                    component={'input'}
+                />
+            </div>
+            <div>
+                <Field
+                    name={'password'}
+                    placeholder={'Login'}
+                    component={'input'}
+                />
+            </div>
+            <div>
+                <Field
+                    name={'rememberMe'}
+                    type={'checkbox'}
+                    component={'input'}
+                >remember me</Field>
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+        </form>
+    );
+};
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm);
 
 export const Login = () => {
+    const onSubmitHandler = (formData: any) => {
+        console.log(formData);
+    };
+
     return (
         <div>
-           <h1>LOGIN</h1>
+            <h1>LOGIN</h1>
+            <LoginReduxForm
+                onSubmit={onSubmitHandler}
+            />
         </div>
     );
 };
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,13 +5,15 @@ import sidebarReducer from './sidebar-reducer';
 import {usersReducer} from './users-reducer';
 import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
 
 const reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 export const store = createStore(reducers, applyMiddleware(thunkMiddleware));
